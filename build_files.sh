c0c4a23041f2df5a996b872ea4cf4256f6964bc7
lucifervb2248 <117747477+lucifervb2248@users.noreply.github.com>
2024-07-14 15:14:51 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,23 @@
+#!/bin/bash
 
-# Build the project
+# Echo for debugging
 echo "Building the project..."
+
+# Ensure pip is available and upgrade it
 python3 -m ensurepip --upgrade
-pip3 install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Install the requirements
-pip3 install --target . --upgrade -r /vercel/path0/requirements.txt
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --target . --upgrade -r /vercel/path0/requirements.txt
 
+# Forcing to use python3.9 if it's available
+python3.9 -m pip install -r /vercel/path0/requirements.txt
+
+# Database migrations
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 /vercel/path0/manage.py makemigrations --noinput
+python3.9 /vercel/path0/manage.py migrate --noinput
 
+# Collect static files
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 /vercel/path0/manage.py collectstatic --noinput --clear
